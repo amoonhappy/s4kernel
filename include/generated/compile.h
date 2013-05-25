4914aa93887a5b6519a754ce3cb41fc0c95b92c6
Jeff Zhou <152318@qq.com>
2013-05-25 09:03:26 -0700

    ARIZONA Dirver Update patch 1

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 2 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Sat May 25 07:30:38 PDT 2013"
+#define UTS_VERSION "#2 SMP PREEMPT Sat May 25 09:01:55 PDT 2013"
 #define LINUX_COMPILE_BY "jeff"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILER "gcc version 4.6.2 20111004 (prerelease) (Linaro GCC 4.6-2011.10) "
