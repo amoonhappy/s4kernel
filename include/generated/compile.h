ea4b4eaa4857eed0aa11505527b28bc349d18037
Jeff Zhou <152318@qq.com>
2013-05-25 04:33:06 -0700

    initial commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 27 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#27 SMP PREEMPT Fri May 24 09:16:26 PDT 2013"
+#define LINUX_COMPILE_BY "jeff"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 4.6.2 20111004 (prerelease) (Linaro GCC 4.6-2011.10) "
